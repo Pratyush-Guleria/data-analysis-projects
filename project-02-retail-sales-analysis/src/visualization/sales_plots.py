c35e5ae3e71c7028f3c4e8ee9ddaefb31b2f00a6
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-08-07 16:01:36 +0530

    feat(visualization): add reusable sub-category sales bar plot

--- project-02-retail-sales-analysis/src/visualization/sales_plots.py
+++ project-02-retail-sales-analysis/src/visualization/sales_plots.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
+# Function 1 
 def plot_category_sales(data):
     """
     Plot total sales by category.
@@ -44,3 +44,47 @@ def plot_category_sales(data):
     plt.tight_layout()
 
     return plt.gcf()
+
+
+# Function 2
+def plot_sub_category_sales(data):
+    """
+    Plot total sales by sub-category
+    """
+
+    plt.figure(figsize = (10, 8))
+
+    sns.barplot(
+        data = data,
+        y = "Sub.Category",
+        x = "Sales",
+        palette = "Set2",
+        hue = "Sub.Category",
+        errorbar = None,
+        legend = False,
+        width = 0.9
+    )
+
+    plt.title(
+        "Highest Sales by Product Sub-Category", 
+        fontsize = 15, 
+        fontweight = "bold", 
+        pad = 15)
+
+    plt.ylabel(
+        "Product Sub-Category", 
+        fontsize = 10, 
+        fontweight = "bold", 
+        labelpad = 15
+    )
+
+    plt.xlabel(
+        "Total Sales Revenue", 
+        fontsize = 10, 
+        fontweight = "bold", 
+        labelpad = 15
+    )
+
+    plt.tight_layout()
+    
+    return plt.gcf()   
