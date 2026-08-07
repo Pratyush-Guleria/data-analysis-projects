0f7785a64dbf6330db77785602b19a7b7d032e30
Pratyush-Guleria <258414203+Pratyush-Guleria@users.noreply.github.com>
2026-08-07 16:01:19 +0530

    feat(analysis): add reusable sub-category sales aggregation

--- project-02-retail-sales-analysis/src/analysis/sales_analysis.py
+++ project-02-retail-sales-analysis/src/analysis/sales_analysis.py
@@ -1,3 +1,4 @@
+# Function 1
 def category_sales(df):
     """
     Returns total sales grouped by category.
@@ -8,4 +9,16 @@ def category_sales(df):
           .sum()
           .sort_values(ascending=False)
           .reset_index()
+    )
+
+
+# Function 2
+def sub_category_sales(df):
+    """
+    Returns total sales grouped by sub-category.
+    """
+    return (df.groupby("Sub.Category")["Sales"]
+            .sum()
+            .sort_values(ascending = False)
+            .reset_index()
     )
